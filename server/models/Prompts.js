6cca897360506c92eb4e538e21b73dfe2702a1cd
UrbanCoffee <rudy.m.7854@gmail.com>
2022-05-25 00:28:12 -0700

    getPrompt API call

---
+++ server/models/Prompts.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PromptSchema = new Schema ({
+    Prompt:{
+        type: String,
+        required: true
+    },
+    id:{
+        type: Number,
+        required: false
+    }
+});
+
+module.exports = Item = mongoose.model('prompts', PromptSchema);
