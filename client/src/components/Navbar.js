3c4ec9d415023e17d3ceb99183b8205c80b4b03e
roveredpwn <roverdog56@gmail.com>
2022-06-01 01:15:54 -0700

    merge ranking framework into profile-comments

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,12 +12,12 @@ class Navbar extends Component {
 					</a>
 				</li>
 				<li>
-					<a href="rankings" className="navbar-link navbar-link-blue">
+					<a href="/rankings" className="navbar-link navbar-link-blue">
 					Rankings
 					</a>
 				</li>
 				<li>
-					<a href="admin" className="navbar-link navbar-link-pink">
+					<a href="/admin" className="navbar-link navbar-link-pink">
 					{/* TODO: Display Admin button conditional based on if logged in account is admin.*/}
 					Admin
 					</a>
@@ -26,12 +26,12 @@ class Navbar extends Component {
 			<ul className="navbar-items-right">
 				{/* TODO: Display login/signup or profile image conditionally based on if user is signed in.*/}
 				<li>
-					<a href="register" className="navbar-link navbar-link-blue">
+					<a href="/register" className="navbar-link navbar-link-blue">
 					Sign Up
 					</a>
 				</li>
 				<li>
-					<a href="login" className="navbar-link navbar-link-blue">
+					<a href="/login" className="navbar-link navbar-link-blue">
 					Log In
 					</a>
 				</li>
