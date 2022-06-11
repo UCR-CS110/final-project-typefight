3ef438bf2e46528bcbc089f4afda4250e72c2a84
roveredpwn <roverdog56@gmail.com>
2022-06-10 23:57:41 -0700

    Fixed incorrect merge conflict changes

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -24,7 +24,9 @@ export default function Comment(props){
   return(
     <div className="comment">
         <a href={props.commenter} className="commenter">
-          <img src={DefaultImage} className="commenter-picture" alt="user"/>
+          <div className="commenter-profile-picture-container">
+				    <ProfilePicture username={props.commenter}/>
+				  </div>
           <div className="commenter-username">{props.commenter}</div>
         </a>
         <p className="text">{props.text}</p>
